3362a0c0602010f8391d19322c4ce7464eb89c93
Tom Houlé <tom@tomhoule.com>
2025-10-10 10:53:17 +0200

    fish/rustup.fish: take $CARGO_HOME into account

--- dot_config/private_fish/conf.d/rustup.fish
+++ dot_config/private_fish/conf.d/rustup.fish
@@ -1,4 +1,8 @@
-set CARGO_ENV_FISH "$HOME/.cargo/env.fish"
+if set -q $CARGO_HOME
+    set CARGO_ENV_FISH "$CARGO_HOME/env.fish"
+else
+    set CARGO_ENV_FISH ~/env.fish
+end
 
 if test -e $CARGO_ENV_FISH
     source $CARGO_ENV_FISH
