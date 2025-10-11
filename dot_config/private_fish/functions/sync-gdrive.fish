7674816f55db9fda831202585ce67136b1af66bf
Tom Houlé <tom@tomhoule.com>
2025-10-11 10:59:49 +0200

    fish: add sync-gdrive function

---
+++ dot_config/private_fish/functions/sync-gdrive.fish
@@ -0,0 +1,24 @@
+function sync-gdrive
+    set directory_name "$(hostname)"
+
+    if test -z $directory_name
+        echo "Could not determine hostname"
+        return 1
+    end
+
+    set source_path ~/Documents/gdrive-outbox
+    set target_path "gdrive-tom:/Inbox/$(hostname)"
+    set sync_command "rclone sync $source_path $target_path"
+
+    echo "Run this? $sync_command"
+    read --local --prompt-str "[y/N]: " confirm
+
+    switch $confirm
+    case "y" "Y"
+        eval $sync_command
+        return $status
+    case '*'
+        echo "Sync cancelled"
+        return 1
+    end
+end
