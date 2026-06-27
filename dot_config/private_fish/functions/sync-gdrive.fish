05dd37d846725da5733ba5b395ee11a544b05a80
Tom Houlé <tom@tomhoule.com>
2026-06-27 13:45:11 +0200

    sync-gdrive: Add download to ~/gdrive

--- dot_config/private_fish/functions/sync-gdrive.fish
+++ dot_config/private_fish/functions/sync-gdrive.fish
@@ -8,8 +8,31 @@ function sync-gdrive
 
     set source_path ~/Documents/gdrive-outbox
     set target_path "gdrive-tom:/Inbox/$(hostname)"
-    set sync_command "rclone sync $source_path $target_path"
+    set sync_command "rclone sync --interactive --progress $source_path $target_path"
 
+    echo "Syncing outbox"
+    echo "Run this? $sync_command"
+    read --nchars 1 --local --prompt-str "[y/N]: " confirm
+
+    switch $confirm
+    case "y" "Y"
+        eval $sync_command
+        if test $status -ne 0
+            echo "Command failed with status $status"
+            return $status
+        end
+    case '*'
+        echo "Sync cancelled"
+        return 1
+    end
+
+    set source_path "gdrive-tom:/"
+    set target_path ~/gdrive
+    set sync_command "rclone sync --interactive --progress $source_path $target_path"
+
+    echo ""
+    echo "Syncing inbox"
+    echo ""
     echo "Run this? $sync_command"
     read --nchars 1 --local --prompt-str "[y/N]: " confirm
 
