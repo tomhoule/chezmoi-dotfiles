da0b42c86500098626a39c99b3563fd05c27c0e2
Tom Houlé <tom@tomhoule.com>
2026-08-31 16:08:26 +0200

    fish: Simplify $EDITOR selection

--- dot_config/private_fish/conf.d/editor.fish
+++ dot_config/private_fish/conf.d/editor.fish
@@ -1,11 +1,9 @@
 if not set -q EDITOR
-    if type -q helix
-        set -gx EDITOR helix
-    else if type -q hx
-        set -gx EDITOR hx
-    else if type -q nvim
-        set -gx EDITOR nvim
-    else if type -q vi
-        set -gx EDITOR vi
-    end
+    if type -q nvim
+		set -gx EDITOR nvim
+	else if type -q vim
+		set -gx EDITOR vim
+	else if type -q vi
+		set -gx EDITOR vi
+	end
 end
