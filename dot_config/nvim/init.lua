ebe47263eb0c027855e535d93b5f298f59b325d7
Tom Houlé <tom@tomhoule.com>
2026-08-31 16:08:26 +0200

    nvim: Set colorscheme to catppuccin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -15,6 +15,8 @@ do
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = false
 
+  vim.cmd.colorscheme("catppuccin")
+
   -- [[ Setting options ]]
   --  See `:help vim.o`
   -- NOTE: You can change these options as you wish!
